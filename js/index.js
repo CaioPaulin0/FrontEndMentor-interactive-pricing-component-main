5a497a1ea9195a69fac20d6c5f29c67294e45336
thegamedenerd <gamedenerd@hotmail.com>
2022-02-07 20:18:30 -0300

    update-value-bar

--- js/index.js
+++ js/index.js
@@ -1,33 +1,73 @@
+
 const pricingValue = document.getElementById('pricingValueRange')
 
 const progressbar = document.getElementById('progressBar')
 
 const slide = document.getElementById('slider')
 
-pricingValue.addEventListener('mousemove', function(){
+const input = document.querySelector('.circle')
 
+pricingValue.addEventListener('mousemove', function(){
     progressbar.style.width = pricingValue.value + '%'
 
     const pricingValueN = Number(pricingValue.value)
+    value(input)
 
-    console.log(pricingValue.value)
-    if(pricingValueN <= 25){
-       pageView('10k') 
-       monthView('$8.00')
-    } else if(pricingValueN <= 50){
-        pageView('50K')
-        monthView('$12.00')
-    } else if(pricingValueN <= 75){
-        pageView('100K')
-        monthView('$16.00')
-    } else if(pricingValueN < 100){
-        pageView('500k')
-        monthView('$24.00')
-    } else if(pricingValueN == 100){
-        pageView('1M')
-        monthView('$35.00')
+})
+
+input.addEventListener('click',function(){
+    if(input.checked){
+        discValue()
+    } else{
+        value()
     }
 })
+
+function value(input){
+
+    const pricingValueN = Number(pricingValue.value)
+
+    if(input.checked){
+        if(pricingValueN <= 25){
+            pageView('10k') 
+            monthView(6)
+         } else if(pricingValueN <= 50){
+             pageView('50K')
+             monthView(9)
+         } else if(pricingValueN <= 75){
+             pageView('100K')
+             monthView(12)
+         } else if(pricingValueN < 100){
+             pageView('500k')
+             monthView(18)
+         } else if(pricingValueN == 100){
+             pageView('1M')
+             monthView(26.25)
+         } 
+
+         return
+
+    } 
+    else{
+        if(pricingValueN <= 25){
+            pageView('10k') 
+            monthView(8)
+         } else if(pricingValueN <= 50){
+             pageView('50K')
+             monthView(12)
+         } else if(pricingValueN <= 75){
+             pageView('100K')
+             monthView(16)
+         } else if(pricingValueN < 100){
+             pageView('500k')
+             monthView(24)
+         } else if(pricingValueN == 100){
+             pageView('1M')
+             monthView(35)
+         }
+    }
+
+}
 
 function pageView(texto){
     const pageView = document.getElementById('pageview')
@@ -40,15 +80,7 @@ function pageView(texto){
 function monthView(value){
     const monthValue = document.getElementById('valueMonth')
 
-    monthValue.innerHTML = value 
+    monthValue.innerHTML = '$' + value.toFixed(2) 
 
     return monthValue
 }
-
-const option = document.querySelector('.monthlyAct')
-const circle = document.querySelector('.circle')
-
-option.addEventListener('click', function(){
-    option.classList.toggle("mActive")
-    circle.classList.toggle("active")
-})
